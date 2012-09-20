ae374e6daf1bbf9285ae85fa8479b07f2b327ebd
Paul <paul.01647+github@gmail.com>
2012-09-20 20:41:57 +0900

    数字位置特定まで書いたはず

--- scoreRecognize/recognize.cpp
+++ scoreRecognize/recognize.cpp
@@ -1,5 +1,5 @@
 /*
-キャラクタのスコアを画像をテキスト及び数値として読み取る
+キャラクタのスコアを，画像を基にテキスト及び数値として読み取る
 */
 
 #include "recognize.h"
@@ -40,27 +40,45 @@ enum Job { JUnknown, JWarrior, JScout, JSorcerer, JFencer, JCestas };
 struct Score
 {
 	// 順位
-	int c_rank;
+	int rank;
+	static const int RankOffset = 0;
+	static const int RankWidth = 14;
 	// キャラクタ名
 	std::string name;
+	static const int NameOffset = 29;
+	static const int NameWidth = 114;
 	// 所属国
 	enum Nationality nationality;
+	static const int NationalityOffset = 180;
+	static const int NationalityWidth = 18;
 	// クラス
 	enum Job job;
+	static const int JobOffset = 225;
+	static const int JobWidth = 84;
 	// キル数
 	int kill;
+	static const int KillOffset = 323;
+	static const int KillWidth = 46;
 	// デッド数
 	int dead;
+	static const int DeadOffset = 386;
+	static const int DeadWidth = 40;
 	// 貢献度
 	int contribution;
+	static const int ContributionOffset = 451;
+	static const int ContributionWidth = 50;
 	// PC与ダメージ
 	int pcDamage;
+	static const int PcDamageOffset = 523;
+	static const int PcDamageWidth = 88;
 	// 建物与ダメージ
 	int objectDamage;
+	static const int ObjectDamageOffset = 629;
+	//static const int ObjectDamageWidth; // 画像右端
 
 	Score()
 	{
-		int c_rank = 0;
+		int rank = 0;
 		name = "774";
 		nationality = NUnknown;
 		job = JUnknown;
@@ -72,12 +90,127 @@ struct Score
 	}
 };
 
+class CharactersInfo
+{
+private:
+	// 文字幅最低要件
+	static const int LowestWidth = 4;
+	// 位置
+	std::vector<int> offsets;
+	// 幅
+	std::vector<int> widths;
+	// 画像
+	cv::Mat image;
+
+public:
+	CharactersInfo( const cv::Mat _image )
+	{
+		// 画像コピー
+		image = _image.clone();
+
+		// 文字存在検索
+		// 輝点列検索フラグ．falseなら暗点列を探す
+		bool flag = true;
+		for ( int col = 0; col < image.cols; col++ )
+		{
+			// 列の輝点数を調べる
+			int nonzero = cv::countNonZero( image.col(col) );
+			if ( flag )
+			{
+				// 輝点列探索中
+				if ( 0 < nonzero )
+				{
+					// 輝点が1個以上あったとき
+					offsets.push_back(col);
+					// フラグ切り替え
+					flag = false;
+				}
+			}
+			else
+			{
+				// 暗点列探索中
+				if ( 0 == nonzero )
+				{
+					// 暗点列だったなら
+					// offsetsの最後の値を参照し，幅を求める
+					int width = col - offsets.back();
+					// LowestWidthを満足するか
+					if ( CharactersInfo::LowestWidth <= width )
+					{
+						// 文字と認める
+						widths.push_back( width );
+					}
+					else
+					{
+						// 文字とは認めない
+						// 直前のoffsetsを除去する
+						offsets.pop_back();
+					}
+					// フラグ切り替え
+					flag = true;
+				}
+			}
+		}
+	}
+
+	// 文字数
+	size_t size()
+	{
+		return offsets.size();
+	}
+
+	// 文字画像
+	cv::Mat characterImage ( size_t n )
+	{
+		// 存在しない文字番号は原画像を返す
+		if ( size() < n )
+		{
+			return image;
+		}
+		
+		return image( cv::Rect( offsets[n], 0, widths[n], image.rows ) );
+	}
+
+};
+
+// 画像から数値を読み出す
+int recognizeNumeric ( const cv::Mat image, const int offset, const int width )
+{
+	// 文字列情報
+	std::vector<struct CharactersInfo> charactersInfo( image );;
+
+	// 数値認識
+	// 連結
+
+	return 0;
+}
+
 // キャラクタスコア認識
 void recognize ( const cv::vector<cv::Mat> images, std::vector<struct Score> &scores )
 {
 	for ( size_t i = 0; i < images.size(); i++ )
 	{
 		struct Score score;
+		
+		// 順位
+		recognizeNumeric( images[i], Score::RankOffset, Score::RankWidth );
+		// キャラクタ名
+		//recognizeText ( images[i], Score::NameOffset, Score::NameWidth );
+		// 所属国
+		//recognizeNationalityIcon ( images[i], Score::NationalityOffset, Score::NationalityWidth );
+		// クラス
+		//recognizeText ( images[i], Score::JobOffset, Score::JobWidth );
+		// キル数
+		recognizeNumeric ( images[i], Score::KillOffset, Score::KillWidth );
+		// デッド数
+		recognizeNumeric ( images[i], Score::DeadOffset, Score::DeadWidth );
+		// 貢献度
+		recognizeNumeric ( images[i], Score::ContributionOffset, Score::ContributionWidth );
+		// PC与ダメージ
+		recognizeNumeric ( images[i], Score::PcDamageOffset, Score::PcDamageWidth );
+		// 建物与ダメージ
+		recognizeNumeric ( images[i], Score::ObjectDamageOffset, images[i].cols - Score::ObjectDamageOffset );
+
 		scores.push_back ( score );
 	}
 
