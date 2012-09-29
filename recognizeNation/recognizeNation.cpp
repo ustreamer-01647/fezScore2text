5e640b50a3e4552ba8bbce34a9beadfcfba4f5b0
Paul <paul.01647+github@gmail.com>
2012-09-29 16:31:07 +0900

    iel netは2値化閾値130.0で適当な差を表した //iel: 7 //net: 23

--- recognizeNation/recognizeNation.cpp
+++ recognizeNation/recognizeNation.cpp
@@ -9,14 +9,35 @@ int main (int argc, char *argv[])
 	cv::Mat ces = cv::imread ( "ces.png", 0 );
 	cv::Mat geb = cv::imread ( "geb.png", 0 );
 	cv::Mat hor = cv::imread ( "hor.png", 0 );
+	cv::Mat iel130 = cv::imread ( "ielbin130.png", 0 );
+	cv::Mat net130 = cv::imread ( "netbin130.png", 0 );
 
 	std::cout << "ces: " << cv::countNonZero(ces) << std::endl;
 	std::cout << "geb: " << cv::countNonZero(geb) << std::endl;
 	std::cout << "hor: " << cv::countNonZero(hor) << std::endl;
+	std::cout << "iel: " << cv::countNonZero(iel130.rowRange(2, iel130.rows)) << std::endl;
+	std::cout << "net: " << cv::countNonZero(net130.rowRange(2, net130.rows)) << std::endl;
 
 	cv::Mat iel = cv::imread ( "iel.png" );
 	cv::Mat net = cv::imread ( "net.png" );
 
+	cv::Mat ag, bg;
+	cv::cvtColor( iel, ag, CV_BGR2GRAY );
+	cv::cvtColor( net, bg, CV_BGR2GRAY );
+
+	cv::Mat ab, bb;
+	cv::threshold( ag, ab, 130.0, 255.0, CV_THRESH_BINARY );
+	cv::threshold( bg, bb, 130.0, 255.0, CV_THRESH_BINARY );
+
+	cv::imshow( "iel", ab );
+	cv::imshow( "net", bb );
+	cv::imwrite("ielbin130.png", ab);
+	cv::imwrite("netbin130.png", bb);
+
+	cv::waitKey();
+
+
+	/* grubCut–³—
 	cv::Mat ielMask = cv::Mat::zeros( iel.size(), CV_8UC1 );
 	// —¼’[1—ñ‚¸‚Â‚ð”wŒiŽw’è
 	for ( int row = 0; row < ielMask.rows; row++ )
@@ -39,6 +60,7 @@ int main (int argc, char *argv[])
 	cv::imshow ( "fg", fgdModel );
 
 	cv::waitKey();
+	*/
 
 	return 0;
 }
@@ -46,3 +68,5 @@ int main (int argc, char *argv[])
 //ces: 25
 //geb: 52
 //hor: 99
+//iel: 7
+//net: 23
