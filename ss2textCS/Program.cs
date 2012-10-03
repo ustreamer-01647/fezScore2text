6f2b464d7897b92715fdb643584732389456872b
Paul <paul.01647+github@gmail.com>
2012-10-03 14:56:08 +0900

    ss2textをCSharpに書き換える． oepncvsharpのチュートリアル通過を確認した． http://code.google.com/p/opencvsharp/wiki/Tutorial_Windows opencv2.4から必要なdllをプロジェクトに与え「新しい場合はコピーする」設定で実行可能になる

---
+++ ss2textCS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using System.Runtime.InteropServices;
+
+namespace ss2textCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (IplImage image = new IplImage(128, 128, BitDepth.U8, 1))
+            {
+                image.Zero();
+                for (int x = 0; x < image.Width; x++)
+                {
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        int offset = y * image.WidthStep + x;
+                        byte value = (byte)(x + y); 
+                        Marshal.WriteByte(image.ImageData, offset, value);
+
+                    }
+                }
+                using (CvWindow window = new CvWindow(image))
+                {
+                    CvWindow.WaitKey();
+                }
+            }
+        }
+    }
+}
